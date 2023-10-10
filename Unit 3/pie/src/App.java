6c79598795ff829b4949ee7c1f062358c7caa63a
Cole-Sieper <24csieper@derryfield.org>
2023-10-10 14:38:09 -0400

    hw 6 part a and b

--- Unit 3/pie/src/App.java
+++ Unit 3/pie/src/App.java
@@ -1,17 +1,33 @@
+// Cole Sieper
+//Hw 6 part A
+
+
 import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
 
         Scanner in = new Scanner(System.in);
 
-        int pies = 0;
+        //inititalize values
+        int pie_order = 0;
         double price_pie = 10.0;
         double discount = 0.9; //10% discount fro people who buy 3 or more pies
+        double subtotal = pie_order*price_pie;
 
+        //inputs
+        System.out.println("Welcome! How many pies would you like? (3 or more get 10% off): ");
+        pie_order = in.nextInt();
 
+        //let customer know they get a discount
+        if (pie_order >= 3) {
 
+            System.out.println("You get 10% off for your order size!");
+            subtotal = subtotal * discount;
+        }
+
+        // let custoemr knwo the total
+        System.out.println("Your order total is: "+"$"+subtotal);
 
     }
 }
