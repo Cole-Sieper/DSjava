fe76f437e9e10a7294bf3d2c598dfbb10aa1a094
Cole-Sieper <24csieper@derryfield.org>
2023-10-10 14:27:12 -0400

    edits to things

---
+++ Unit 3/pie/src/App.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+
+        Scanner in = new Scanner(System.in);
+
+        int pies = 0;
+        double price_pie = 10.0;
+        double discount = 0.9; //10% discount fro people who buy 3 or more pies
+
+
+
+
+    }
+}
