b990407555470b313d19a83fe67fdc6e66910daf
Cole-Sieper <24csieper@derryfield.org>
2023-11-02 09:30:37 -0400

    HE 9 at part d finished

--- Unit 4/HW9/AT Part D/src/App.java
+++ Unit 4/HW9/AT Part D/src/App.java
@@ -1,5 +1,73 @@
+// Cole Sieper
+// 11/1/23
+// HW 9 Part D
+
+class Die {
+    public int roll() {
+        return (int) (Math.random() * 6) + 1;
+    }
+
+    public void display() {
+
+        if (roll()==1) {
+        System.out.println("+-------+");
+        System.out.println("|       |");
+        System.out.println("|   *   |");
+        System.out.println("|       |");
+        System.out.println("+-------+");
+        }
+        if (roll()==2) {
+        System.out.println("+-------+");
+        System.out.println("| *     |");
+        System.out.println("|       |");
+        System.out.println("|     * |");
+        System.out.println("+-------+");
+        }
+        if (roll()==3) {
+        System.out.println("+-------+");
+        System.out.println("| *     |");
+        System.out.println("|   *   |");
+        System.out.println("|     * |");
+        System.out.println("+-------+");
+        }
+        if (roll()==4) {
+        System.out.println("+-------+");
+        System.out.println("| *   * |");
+        System.out.println("|       |");
+        System.out.println("| *   * |");
+        System.out.println("+-------+");
+        }
+        if (roll()==5) {
+        System.out.println("+-------+");
+        System.out.println("| *   * |");
+        System.out.println("|   *   |");
+        System.out.println("| *   * |");
+        System.out.println("+-------+");
+        }
+        if (roll()==6) {
+        System.out.println("+-------+");
+        System.out.println("| *   * |");
+        System.out.println("| *   * |");
+        System.out.println("| *   * |");
+        System.out.println("+-------+");
+        }
+    }
+}
+
+
+
+
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        
+        Die die = new Die();
+
+        for (int i = 1; i < 7; i++) {
+        die.roll();
+        die.display();
+
+        }
+
     }
 }
