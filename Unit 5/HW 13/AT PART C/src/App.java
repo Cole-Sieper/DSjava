c5cc699e1415fd6e99f17be632032898ea00700c
Cole-Sieper <24csieper@derryfield.org>
2023-12-06 11:41:15 -0500

    HW 13 AT part C flower drawing finished

--- Unit 5/HW 13/AT PART C/src/App.java
+++ Unit 5/HW 13/AT PART C/src/App.java
@@ -2,70 +2,56 @@
 // 12/5/23
 // HW 13 Part C
 
-
-
 import java.awt.Color;
 import java.awt.Graphics;
 import javax.swing.*;
 
-
 public class App {
 
+   public static void draw(Graphics g) {
 
-    
+      g.setColor(Color.BLACK);
 
+      final int STEPS = 1000;
 
-   public static void draw(Graphics g)
-   {  
+      double delta = 2 * Math.PI / STEPS;
 
-   g.setColor(Color.BLACK);
+      double theta = 0;
+      int scale = 150;
 
-  
-    for (int steps = 0;steps <= 1000; steps++) {
+      for (int i = 0; i <= STEPS; i++) {
 
-         int scale = 150;
+         theta = theta + delta;
 
-        double delta = 2*Math.PI;
+         double r = Math.cos(2 * theta);
 
-        double theta = (delta/steps);
+         double y = scale * r * Math.sin(theta);
+         double x = scale * r * Math.cos(theta);
 
-        double r = Math.cos(2*theta);
-        
-        double y = scale * r * Math.sin(theta);
-        double x = scale * r * Math.cos(theta);
+         int plotY = (int) y + 199;
+         int plotX = (int) x + 199;
 
-        int plotY = (int)y;
-        int plotX = (int)x;
+         g.drawOval(plotX, plotY, 1, 1);
 
-        g.drawOval(plotX, plotY, 1, 1);
-
-
-         
-
-
-    }
-      
-
+      }
 
    }
-   public static void main(String[] args) 
-   {
-      final int FRAME_WIDTH = 500;
-      final int FRAME_HEIGHT = 500;
+
+   public static void main(String[] args) {
+      final int FRAME_WIDTH = 400;
+      final int FRAME_HEIGHT = 400;
 
       JFrame frame = new JFrame();
       frame.setSize(FRAME_WIDTH, FRAME_HEIGHT);
       frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-      JComponent component = new JComponent()
-         {
-            public void paintComponent(Graphics graph)
-            {
-               draw(graph);
-            }
-         };
-      
+      JComponent component = new JComponent() {
+         public void paintComponent(Graphics graph) {
+            draw(graph);
+         }
+      };
+
       frame.add(component);
       frame.setVisible(true);
-   }   
+   }
 }
