f33564f918f6f5ac9c5709c17d7656aeb1158801
Cole-Sieper <24csieper@derryfield.org>
2023-12-08 10:22:42 -0500

    Programming project 5 Project 2 Finished
    fixed the problem of 0 not working for first input by switching the initial input to a string

--- Unit 5/Programing Project 5/src/ProjectTwo.java
+++ Unit 5/Programing Project 5/src/ProjectTwo.java
@@ -11,7 +11,9 @@ public class ProjectTwo {
         Scanner in = new Scanner(System.in);
         
         System.out.print("Please enter a zip code: ");
-        int zipcode = in.nextInt();
+        String zipcode = in.nextLine();
+
+
 
         printBarCode(zipcode);
 
@@ -58,55 +60,27 @@ public class ProjectTwo {
 
 
         
-/* 
-        //print out each digit
-            if (d == 1) {
-            return ":::||";
-            }
-            if (d == 2) {
-            return "::|:|";
-            }
-            if (d == 3) {
-            return "::||:";
-            }
-            if (d == 4) {
-            return ":|::|";
-            }
-            if (d == 5) {
-            return ":|:|:";
-            }
-            if (d == 6) {
-            return ":||::";
-            }
-            if (d == 7) {
-            return "|:::|";
-            }
-            if (d == 8) {
-            return "|::|:";
-            }
-            if (d == 9) {
-            return "|:|::";
-            }
-            if (d == 0) {
-            return "||:::";
-            }
 
-            */
-       
         
 
     }
-    public static int getCheckDigit(int zipCode) {
+    public static int getCheckDigit(String zipCode) {
 
         int checkDigit = 0;
 
-        String newZip = Integer.toString(zipCode);
+        String newZip = zipCode;
 
-        int n1 = Integer.valueOf(newZip.charAt(0));
-        int n2 = Integer.valueOf(newZip.charAt(1));
-        int n3 = Integer.valueOf(newZip.charAt(2));
-        int n4 = Integer.valueOf(newZip.charAt(3));
-        int n5 = Integer.valueOf(newZip.charAt(4));
+        String s1 = newZip.substring(0,1);
+        String s2 = newZip.substring(1,2);
+        String s3 = newZip.substring(2,3);
+        String s4 = newZip.substring(3,4);
+        String s5 = newZip.substring(4);
+
+        int n1 = Integer.valueOf(s1);
+        int n2 = Integer.valueOf(s2);
+        int n3 = Integer.valueOf(s3);
+        int n4 = Integer.valueOf(s4);
+        int n5 = Integer.valueOf(s5);
 
         int total = n1+n2+n3+n4+n5;
 
@@ -144,9 +118,9 @@ public class ProjectTwo {
 
 
     }
-    public static void printBarCode(int zipCode) {
+    public static void printBarCode(String zipCode) {
 
-        String newZip = Integer.toString(zipCode);
+        String newZip = zipCode;
 
         String s1 = newZip.substring(0,1);
         String s2 = newZip.substring(1,2);
@@ -160,22 +134,7 @@ public class ProjectTwo {
         int n4 = Integer.valueOf(s4);
         int n5 = Integer.valueOf(s5);
 
-        
-        System.out.println(s1);
-        System.out.println(s2);
-        System.out.println(s3);
-        System.out.println(s4);
-        System.out.println(s5);
-
-        /* 
-        String sec1 = getEncodedDigit(n1);
-        String sec2 = getEncodedDigit(n2);
-        String sec3 = getEncodedDigit(n3);
-        String sec4 = getEncodedDigit(n4);
-        String sec5 = getEncodedDigit(n5);
-        String checkDigit = getEncodedDigit(getCheckDigit(zipCode));
-
-        */
+       
 
         System.out.print("|");
         getEncodedDigit(n1);
