bd206ee50f3f332e0f3b7cf0de8aadfcc9ea8332
Cole-Sieper <24csieper@derryfield.org>
2023-12-08 10:37:01 -0500

    HW 14 arrays done

---
+++ Unit 6/HW 14 Arrays/src/App.java
@@ -0,0 +1,22 @@
+// Cole Sieper
+// 12/8/23
+// HW 14 Arrays 
+
+
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        
+
+        double[] numbers = { 2.2, 1.0, 3.7, 4.1, 88.0 } ;
+
+        for (int i = 0; i < numbers.length; i++) {
+
+            System.out.println(numbers[i]);
+
+        }
+
+
+
+    }
+}
