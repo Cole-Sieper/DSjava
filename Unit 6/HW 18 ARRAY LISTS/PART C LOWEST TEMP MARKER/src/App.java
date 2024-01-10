04f24edc644c78f6c75c9df481a2b2d87ee20002
Cole-Sieper <24csieper@derryfield.org>
2024-01-10 11:22:31 -0500

    HW 18 Part C finished

---
+++ Unit 6/HW 18 ARRAY LISTS/PART C LOWEST TEMP MARKER/src/App.java
@@ -0,0 +1,101 @@
+// Cole SIeper
+// 1/10/24
+// HW 18 Part C
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        
+        Scanner in = new Scanner(System.in);
+
+
+        ArrayList<Integer> list = new ArrayList<Integer>();
+
+
+        // input loop
+
+        // input loop
+
+        System.out.println ("--- Enter 'q' to end input ---");
+
+        while (true) 
+        {
+
+            String input = "";
+
+            
+            System.out.print("Plese enter a integer: ");
+            input = in.nextLine();
+
+            if (input.charAt(0)== 'q' || input.charAt(0)=='Q') {
+                System.out.println("Done entering inputs. ");
+
+                break;
+
+            } else {
+
+                //convert string to an double
+
+                int input_num = Integer.valueOf(input);
+
+                // Save the integer to a the array
+
+                list.add(input_num);
+            
+                
+                }
+
+            }
+
+
+            
+
+
+            // Loop to find the lowest num
+            int lowestNum = list.get(0);
+
+            for (int i = 0; i < list.size(); i++) {
+
+
+                if ( lowestNum >= list.get(i)) {
+
+                    lowestNum = list.get(i);
+
+                }
+
+
+            }
+
+            // --- Output Loop ---
+
+            for (int i = 0; i < list.size(); i++) {
+
+                if (list.get(i)== lowestNum) {
+
+                    System.out.println(list.get(i)+" <= Lowest");
+
+                } else {
+
+                    System.out.println(list.get(i));
+
+                }
+
+            }
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
