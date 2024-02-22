a218cb8532ebe5474e771ef79e6734778dd554a5
Cole-Sieper <24csieper@derryfield.org>
2024-02-22 10:14:41 -0500

    HW 24 part a completed

---
+++ Unit 8/HW 24/PartA.java
@@ -0,0 +1,39 @@
+// Cole Sieper
+// 2/22/24
+// HW 24 Part A
+
+import java.util.Scanner;
+
+public class PartA {
+
+public static void main(String[] args) {
+    
+
+getName();
+
+
+
+
+
+
+}
+
+public static void getName() {
+
+    Scanner in = new Scanner(System.in);
+
+    System.out.print("Please enter your name: ");
+    String name = in.nextLine();
+
+    System.out.println("Your name is "+name);
+
+    System.out.print("Plese enter your age: ");
+    int age = in.nextInt();
+
+    System.out.println("Your age is "+age);
+
+
+
+}
+
+}
---
+++ Unit 8/HW 24/PartB.java
@@ -0,0 +1,40 @@
+// Cole Sieper
+// 2/22/24
+// HW 24 Part B
+
+class Counter {
+
+
+public int count(int counter) {
+
+counter = counter + 1;
+
+return counter;
+
+}
+
+
+
+
+
+
+}
+
+
+public class PartB {
+    
+
+    public static void main(String[] args) {
+
+        Counter tally = new Counter();
+
+        int cake = 1;
+
+        tally.count(cake);
+
+        System.out.println(cake);
+
+        
+        
+    }
+}
